db131ca00d9e51c0fe31124711079b030fe0a6f4
Jaymel <qmflr79@kookmin.ac.kr>
2017-09-07 09:44:16 +0900

    second task

--- week2/20171662-이동윤-assignment2.py
+++ week2/20171662-이동윤-assignment2.py
@@ -3,7 +3,11 @@ num = int(input("Enter your number : "))
 
 while num != -1 :
     sum = 1
-    for mul in range(1,num+1) :
-        sum *= mul
-    print("%d! = %d" %(num,sum))
-    num = int(input("Enter your number : "))
+    if num >= 0 :
+        for mul in range(1,num+1) :
+            sum *= mul
+        print("%d! = %d" %(num,sum))
+        num = int(input("Enter your number : "))
+    else :
+        print("정수를 입력해주세요!")
+        num = int(input("Enter your number : "))
