567ce17de42cb9a158ec7d45d5e05b58427c6451
Jaymel <qmflr79@kookmin.ac.kr>
2017-09-26 09:22:49 +0900

    advanced4

---
+++ week4/20171662-이동윤-advanced4.py
@@ -0,0 +1,16 @@
+TF = 1
+
+def Factorial(fact, k):
+    if k == 0:
+        return fact
+    return Factorial(fact * k, k - 1)
+
+while TF:
+    n = int(input("Enter number n: "))
+    r = int(input("Enter number r: "))
+
+    if n >= 0 and r >= 0:
+        print("nCr : ", int(Factorial(1,n)/(Factorial(1,r)*Factorial(1,n-r))))
+    else:
+        print("정수를 입력해주세요!")
+        TF=0
